a8840ab0df02f9a76b545d9921ffb9c82d84dcb2
kimjunwoo <mays990330@naver.com>
2022-03-13 17:04:03 +0900

    1182

--- Silver/실2 1182.py
+++ Silver/실2 1182.py
@@ -1,20 +1,15 @@
+import itertools
+import sys
+from itertools import combinations
+from re import A
+import re
+input = sys.stdin.readline
 n,s=map(int,input().split())
 array=list(map(int,input().split()))
 k=0
-for i in range(n):
-    if sum(array[0:i])==s:
-        k+=1
-for i in range(n):
-    if sum(array[0:i])==s:
-        k+=1
-for i in range(n):
-    if sum(array[0:i])==s:
-        k+=1
-for i in range(n):
-    if sum(array[0:i])==s:
-        k+=1
+for i in range(1,n+1):
+    a=combinations(array,i)
+    for x in a:
+        if sum(x)==s:
+            k+=1
 print(k)
-
-def fuct(x):
-    if sum(array[x:i])==s:
-        k+=1
